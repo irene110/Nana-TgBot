f554d2132884803a41e658bd23e5f15063060ab0
irene110 <60791635+irene110@users.noreply.github.com>
2020-02-08 01:18:58 +0700

    Update and rename config.example.py to config.py

--- nana/config.py
+++ nana/config.py
@@ -4,9 +4,9 @@ class Config(object):
 	
 	# Must be filled!
 	# Register here: https://my.telegram.org/apps
-	api_id = 12345
-	api_hash = "123456789abcdefghijklmnopqrstuvw"
-	DB_URL = "postgres://username:password@localhost:5432/database" # Your database URL
+	api_id = 1013887
+	api_hash = "6e7104cc3413ed5f41bc20e9847ba259"
+	DB_URL = "postgres://zyjmylssbrunyl:126f49cc3d70cbbc0d8484ac04ca08222bc5740d5392faec0594f0962bc9a517@ec2-52-203-160-194.compute-1.amazonaws.com:5432/d96pmc9b6rooa9" # Your database URL
 
 	# Version
 	lang_code = "en" # Your language code
@@ -16,12 +16,12 @@ class Config(object):
 	# Use real bot for Assistant
 	# Pass False if you dont want
 	ASSISTANT_BOT = True
-	ASSISTANT_BOT_TOKEN = ""
+	ASSISTANT_BOT_TOKEN = "1012265438:AAGDk4AsiwFdo0zEfyaAaJET6QcFydm2Myg"
 
 	# Required for some features
 	# Owner and AdminSettings is for your Assistant bot only
-	Owner = 388576209 # Insert your Telegram ID (go @EmiliaHikariBot, type /id)
-	AdminSettings = [388576209] # Do like above, can insert multiple other user id, example [12345, 23456]
+	Owner = 964530719 # Insert your Telegram ID (go @EmiliaHikariBot, type /id)
+	AdminSettings = [964530719] # Do like above, can insert multiple other user id, example [12345, 23456]
 	Command = ["!", "."] # Insert command prefix, if you insert "!" then you can do !ping
 	OutputDownload = "downloads/" # leave it blank if you want to current dir instead
 
